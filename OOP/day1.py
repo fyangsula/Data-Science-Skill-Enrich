b6f3273eff5988d3afc5f67eaeb8c6c786b12b1e
Fan Yang <fan@truex.com>
2018-06-26 22:47:29 -0700

    Learn the concept of class, instance, attribute and method.

--- OOP/day1.py
+++ OOP/day1.py
@@ -1,7 +1,8 @@
-class MyFirstClass:
-    pass
-
-a = MyFirstClass()
-b = MyFirstClass()
-print(a)
-print(b)
+class student:
+    def __init__(self,name,gender,nationality):
+        self.name = name
+        self.gender = gender
+        self.nationality = nationality
+    
+    def self_intro(self):
+        print 'Hello Friends. My name is {}. I am from {}.'.format(self.name, self.nationality)
